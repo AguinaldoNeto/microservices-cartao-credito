b4070954bbce83d79b5bd29cce50fb35e357cca1
Aguinaldo Neto <neto_aguinaldo@outlook.com>
2022-08-30 17:47:19 -0300

    Criando o microservice de Cartões.

---
+++ mscartoes/src/main/java/io/github/aguinaldoneto/mscartoes/application/CartoesResource.java
@@ -0,0 +1,39 @@
+package io.github.aguinaldoneto.mscartoes.application;
+
+import io.github.aguinaldoneto.mscartoes.application.representation.CartaoSaveRequest;
+import io.github.aguinaldoneto.mscartoes.domain.Cartao;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("cartoes")
+@RequiredArgsConstructor
+public class CartoesResource {
+
+    private final CartaoService service;
+
+    @GetMapping
+    public String status() {
+        return "ok";
+    }
+
+    @PostMapping
+    public ResponseEntity cadastra(@RequestBody CartaoSaveRequest request) {
+
+        Cartao cartao = request.toModel();
+        service.save(cartao);
+
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping(params = "renda")
+    public ResponseEntity<List<Cartao>> getCartoesComRendaAte(@RequestParam("renda") Long renda) {
+        List<Cartao> list = service.getCartoesRendaMenorIgual(renda);
+        return ResponseEntity.ok(list);
+
+    }
+}
